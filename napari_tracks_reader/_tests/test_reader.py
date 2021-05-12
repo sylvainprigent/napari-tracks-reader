6888fcccc3f54a56e5979be031fbc577a3587d75
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2021-05-12 17:39:55 +0200

    add ICY, ISBI and csv readers

--- napari_tracks_reader/_tests/test_reader.py
+++ napari_tracks_reader/_tests/test_reader.py
@@ -3,15 +3,23 @@ from napari_tracks_reader import napari_get_reader
 
 
 # tmp_path is a pytest fixture
-def test_reader(tmp_path):
+def test_reader_trackmate(tmp_path):
     """An example of how you might test your plugin."""
 
-    # write some fake data using your supported file format
+    # test the reader is callable
     my_test_file = str(tmp_path / "FakeTracks_TrackMate.xml")
-
-    # try to read it back in
     reader = napari_get_reader(my_test_file)
     assert callable(reader)
+
+    # make sure we're delivering the right format
+    layer_data_list = reader(my_test_file)
+    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
+    layer_data_tuple = layer_data_list[0]
+    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
+
+    # make sure data are read
+    np.testing.assert_equal( () )
+    layer_data_tuple[0].shape
 
 
 def test_get_reader_pass():
