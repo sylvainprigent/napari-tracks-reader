e31fbf6a5757708954aa9522c6ac03ab3caf7883
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2021-05-12 18:03:04 +0200

    add unit testing

--- napari_tracks_reader/_tests/test_reader.py
+++ napari_tracks_reader/_tests/test_reader.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from napari_tracks_reader import napari_get_reader
 
@@ -7,7 +8,8 @@ def test_reader_trackmate(tmp_path):
     """An example of how you might test your plugin."""
 
     # test the reader is callable
-    my_test_file = str(tmp_path / "FakeTracks_TrackMate.xml")
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    my_test_file = os.path.join(root_dir, 'FakeTracks_TrackMate.xml')
     reader = napari_get_reader(my_test_file)
     assert callable(reader)
 
@@ -18,8 +20,68 @@ def test_reader_trackmate(tmp_path):
     assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
 
     # make sure data are read
-    np.testing.assert_equal( () )
-    layer_data_tuple[0].shape
+    np.testing.assert_equal((200, 5), layer_data_tuple[0].shape)
+    np.testing.assert_equal(8, len(layer_data_tuple[1]['graph']))
+
+
+def test_reader_icy(tmp_path):
+    """An example of how you might test your plugin."""
+
+    # test the reader is callable
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    my_test_file = os.path.join(root_dir, 'FakeTracks_Icy.xml')
+    reader = napari_get_reader(my_test_file)
+    assert callable(reader)
+
+    # make sure we're delivering the right format
+    layer_data_list = reader(my_test_file)
+    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
+    layer_data_tuple = layer_data_list[0]
+    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
+
+    # make sure data are read
+    np.testing.assert_equal((237, 5), layer_data_tuple[0].shape)
+    np.testing.assert_equal(9, len(layer_data_tuple[1]['graph']))
+
+
+def test_reader_isbi(tmp_path):
+    """An example of how you might test your plugin."""
+
+    # test the reader is callable
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    my_test_file = os.path.join(root_dir, 'FakeTracks_ISBI.xml')
+    reader = napari_get_reader(my_test_file)
+    assert callable(reader)
+
+    # make sure we're delivering the right format
+    layer_data_list = reader(my_test_file)
+    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
+    layer_data_tuple = layer_data_list[0]
+    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
+
+    # make sure data are read
+    np.testing.assert_equal((156, 5), layer_data_tuple[0].shape)
+    np.testing.assert_equal(0, len(layer_data_tuple[1]['graph']))
+
+
+def test_reader_csv(tmp_path):
+    """An example of how you might test your plugin."""
+
+    # test the reader is callable
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    my_test_file = os.path.join(root_dir, 'two_tracks.csv')
+    reader = napari_get_reader(my_test_file)
+    assert callable(reader)
+
+    # make sure we're delivering the right format
+    layer_data_list = reader(my_test_file)
+    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
+    layer_data_tuple = layer_data_list[0]
+    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
+
+    # make sure data are read
+    np.testing.assert_equal((29, 5), layer_data_tuple[0].shape)
+    np.testing.assert_equal(0, len(layer_data_tuple[1]['graph']))
 
 
 def test_get_reader_pass():
